42be3ef94da5736424f4561edbf4bb58673c3b1a
Nouredine LAMAMRA <noreddine.lamamra@zsoft-consulting.com>
2022-10-22 16:39:16 +0200

    Add the hpp declarations

---
+++ Edge.hpp
@@ -0,0 +1,26 @@
+#ifndef ARETE_H
+#define ARETE_H
+#include "Vertex.hpp"
+
+
+class Edge{
+    private : 
+        int weight;
+        const Vertex source;
+        const Vertex destination;
+
+    public : 
+        Edge(string, string, int);
+        Edge(Vertex&, Vertex&, int);
+        Edge(const Edge&);
+        int getWeight();
+        void setWeight();
+        Vertex getSource();
+        Vertex getDestination();
+
+
+};
+ostream& operator<<(ostream &out, const Edge &x);
+
+
+#endif
---
+++ Vertex.hpp
@@ -0,0 +1,23 @@
+#ifndef SOHHET_H
+#define Vertex_H
+#include<iostream>
+using namespace std;
+
+class Vertex{
+
+    private:
+        const string etiquette;
+        int indx;
+    public : 
+        Vertex(string);
+        Vertex(const Vertex&);
+        string get_etiquette();
+        int get_indx();
+        void set_indx(int);
+};
+
+ostream& operator<<(ostream &out, const Vertex &x);
+
+
+
+#endif
