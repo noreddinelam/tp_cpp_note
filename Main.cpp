43716551bcdab4510fbc7e3702121e89791a069c
kenzaOut16 <outayebkenza@gmail.com>
2022-10-24 02:13:35 +0200

    Add kruskal function

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,9 @@
 #include "Graph.hpp"
 #include "GC.hpp"
 
+
+
+
 int main(int argc, char const *argv[])
 {
     // TEST 01 :
@@ -9,14 +12,16 @@ int main(int argc, char const *argv[])
     Vertex* vertexB = new Vertex("B");
     Vertex* vertexC = new Vertex("C");
     Vertex* vertexD = new Vertex("D");
+    Vertex* vertexE = new Vertex("E");
 
-    Edge* edgeAB = new Edge(vertexA, vertexB, 2);
-    Edge* edgeAC = new Edge(vertexA, vertexC, 3);
-    Edge* edgeAD = new Edge(vertexA, vertexD, 1);
-    Edge* edgeCD = new Edge(vertexC, vertexD, 8);
-    Edge* edgeBD = new Edge(vertexB, vertexD, 5);
+    Edge* edgeAB = new Edge(vertexA, vertexB, 1);
+    Edge* edgeAC = new Edge(vertexA, vertexC, 2);
+    Edge* edgeAD = new Edge(vertexA, vertexD, 2);
+    Edge* edgeEC = new Edge(vertexE, vertexC, 12);
+    Edge* edgeBC = new Edge(vertexB, vertexC, 3);
+    Edge* edgeCD = new Edge(vertexC, vertexD, 24);
     
-    set<Edge *> edges{edgeAB, edgeAC, edgeAD, edgeCD, edgeBD};
+    set<Edge *> edges{edgeAB,edgeAC, edgeBC, edgeCD};
     
     set<Vertex *> vertices{vertexA, vertexB, vertexC, vertexD};
 
@@ -25,11 +30,16 @@ int main(int argc, char const *argv[])
     gc->addEdges(edges);
     gc->addVertices(vertices);
 
-    cout << graph.poids() << endl;
+   // cout << graph.poids() << endl;
 
     graph.symetrise();
+    
+    set<Edge*> res = graph.kruskal();
+     for (set<Edge *>::iterator itr = res.begin(); itr != res.end(); itr++) {
+        cout << *(*itr)<<endl;
+    }
 
-    cout << graph.poids() << endl;
+    //cout << graph.poids() << endl;
 
     delete gc;
     return 0;
